b891c22185de8d9d88bd72c1089032cc730b01c4
Adrienne F. McFaul <adriennemcfaul@gmail.com>
2019-10-23 10:30:47 -0500

    fixed category dropdown menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'storefront/all_items'
 
-  get 'categorical' => 'storefront/items_by_category'
 
-  get 'branding' => 'storefront/items_by_brand'
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
 
   root 'storefront#all_items'
   resources :categories
